2accc2081a88a14eb5f8d5ad45afd914b68c9287
ShopiMax <jarlmaxarv@gmail.com>
2023-08-15 18:58:37 -0400

    Base app created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Dad jokes, when you get help getting it up (in your your head).
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
