2384b2c4c0cbd0b9e529b9b29217f42e612a90f9
ShopiMax <jarlmaxarv@gmail.com>
2023-08-26 20:21:31 -0400

    Basic design update, next get AI image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import React from "react";
 import Title from "./components/Title";
-import JokeButton from './components/JokeButton';
-import Footer from './components/Footer';
+import JokeButton from "./components/JokeButton";
+import Footer from "./components/Footer";
 
- 
 function App() {
   return (
-    <div className="text-bg-primary p-3">
+    <div className="text-bg-primary p-3 App">
       <Title />
-      <JokeButton />
-      <Footer />
+      <div className='joke'>
+        <JokeButton />
+      </div>
+      <div className="Footer">
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
