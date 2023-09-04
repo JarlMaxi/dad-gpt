7c0ba780776f74cf389f8671f264417503175bc8
ShopiMax <jarlmaxarv@gmail.com>
2023-09-04 19:30:54 -0400

    Updated jokebutton

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const OpenAI = require("openai");
 const cors = require("cors");
 
 const openai = new OpenAI({
-  apiKey: process.envOPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const app = express();
