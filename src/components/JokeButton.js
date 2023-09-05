e5a515f09cf6561bbe34b413053155af84a84637
ShopiMax <jarlmaxarv@gmail.com>
2023-09-04 20:22:18 -0400

    updated jokebutton

--- src/components/JokeButton.js
+++ src/components/JokeButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 import axios from "axios";
-import './JokeButton.css'
+import "./JokeButton.css";
 
 const { useState } = require("react");
-
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function JokeButton() {
   const [joke, setJoke] = useState("");
@@ -11,18 +11,20 @@ function JokeButton() {
 
   const fetchJoke = async () => {
     try {
-        const response = await axios.get(`https://dad-joke-494592f7bea7.herokuapp.com/dad-joke`);
-        setJoke(response.data.joke);
-        setShowButton(false);
+      const response = await axios.get(`${backendUrl}/dad-joke`);
+      setJoke(response.data.joke);
+      setShowButton(false);
     } catch (error) {
-        console.error('Error fetching joke:', error);
+      console.error("Error fetching joke:", error);
     }
-};
+  };
 
-return (
-    <div className='joke'>
+  return (
+    <div className="joke">
       {showButton ? (
-        <button className='btn btn-secondary' onClick={fetchJoke}>Get a Dad Joke!</button>
+        <button className="btn btn-secondary" onClick={fetchJoke}>
+          Get a Dad Joke!
+        </button>
       ) : (
         <h3>{joke}</h3>
       )}
